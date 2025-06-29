d6b4f862223d01e917c77f7889dd0853f5c21f23
ChinmayNoob <chinmaysk0408@gmail.com>
2025-06-29 15:44:32 +0530

    fix: hoizontal scrollbar fixed

--- components/navbar/navbar-component.tsx
+++ components/navbar/navbar-component.tsx
@@ -9,23 +9,14 @@ import NavbarNavigation from "./navigation";
 const NavbarComponent = () => {
     return (
         <>
-            <header className="w-full bg-black py-6 relative !z-10">
-                <div className="hidden xl:block absolute top-0 left-0 w-1/2 h-full">
-                    <Image
-                        src="/assets/blurs/white.png"
-                        alt="Blur Branco"
-                        width={64}
-                        height={64}
-                        className="absolute top-[-350px] left-[-190px] !w-full h-[850px] object-cover opacity-70 mix-blend-lighten"
-                    />
-                </div>
+            <header className="w-full bg-black py-6 relative !z-10 overflow-hidden">
                 <div className="hidden xl:block absolute top-0 right-0 w-full h-full">
                     <Image
                         src="/assets/blurs/purple.png"
                         alt="Blur Roxo"
                         width={64}
                         height={64}
-                        className="absolute top-[-100px] right-[-240px] !w-[600px] h-[600px] object-cover opacity-60 mix-blend-lighten"
+                        className="absolute top-[-100px] right-[-150px] !w-[600px] h-[600px] object-cover opacity-60 mix-blend-lighten"
                     />
                 </div>
                 <div className="flex flex-wrap ml-10 md:ml-0 gap-4 xs:gap-0 items-center justify-between xl:justify-between py-4 h-fit px-4 md:px-24 ">
